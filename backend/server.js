c88a4924711f118708cbceaf18b86d79badf35dc
A04987 <gasidit0anu@gmail.com>
2025-06-22 14:48:06 +0700

    Admin+backend test1

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const productsRoute = require('./routes/products');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/api', productsRoute);
+
+
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, '../admin.html')); 
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
